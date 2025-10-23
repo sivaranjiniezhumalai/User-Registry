e4a3e1983ca53cf31c04001d5a5c4e3519f2b00c
sivaranjiniezhumalai <sivaranjiniezhumalai2005@gmail.com>
2025-10-23 19:28:54 +0530

    Add frontend and backend folders to main repo

---
+++ fullstack-backend/fullstack-backend/src/main/java/com/codewithranjini/fullstack_backend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.codewithranjini.fullstack_backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(Long id){
+        super("could not found the user with id" + id);
+    }
+}
---
+++ fullstack-backend/fullstack-backend/src/main/java/com/codewithranjini/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codewithranjini.fullstack_backend.repository;
+
+import com.codewithranjini.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
